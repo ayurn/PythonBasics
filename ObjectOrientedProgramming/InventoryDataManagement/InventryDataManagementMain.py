7d696d957a8f58bcb62e0347f3f35920ca248124
ayurn <ninaweayur@gmail.com>
2021-07-01 22:59:46 +0530

    [Ayur] Refactor : Implemented more OOPs concepts in inventory program

--- ObjectOrientedProgramming/InventoryDataManagement/InventryDataManagementMain.py
+++ ObjectOrientedProgramming/InventoryDataManagement/InventryDataManagementMain.py
@@ -2,16 +2,14 @@
 @Author: Ayur Ninawe
 @Date: 2021-06-30 20:10:30
 @Last Modified by: Ayur Ninawe
-@Last Modified time: 2021-06-30 20:10:30
+@Last Modified time: 2021-07-01 17:10:30
 @Title : Create a JSON file having Inventory Details for Rice, Pulses and Wheats
 with properties name, weight, price per kg.
 """
 
-# Program for read the data from json file and print all data
-
 import InventryDataManagement as jsonObj
 
 if __name__ == "__main__":
-    data = jsonObj.read_File()
-    jsonObj.print_data(data)
-    jsonObj.print_total_price(data)
+    data = jsonObj.Inventory.read_File(open)
+    jsonObj.Inventory.print_data(data)
+    jsonObj.Inventory.print_total_price(data)
