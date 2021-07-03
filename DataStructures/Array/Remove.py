d29879d06ff4928fc927384253607949d58fff26
ayurn <ninaweayur@gmail.com>
2021-07-04 00:12:22 +0530

    [Ayur] Refactor : Write a Python program to remove the first occurrence of a specified element from an arry

---
+++ DataStructures/Array/Remove.py
@@ -0,0 +1,20 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-03 18:25:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-03 18:25:30
+@Title : Write a Python program to remove the first occurrence of a specified element from an
+        array.
+"""
+
+import array as arr 
+
+class Arrayelement():
+    def display(self):
+        ar = arr.array('i', [1, 3, 5, 3, 7, 1, 9, 3])
+        ar.remove(3)
+        print(ar)
+     
+        
+if __name__ == '__main__':
+    Arrayelement.display(0)
