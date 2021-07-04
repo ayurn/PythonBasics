4954c662d6b5f5854a8cf6a5d84d5cb4a224e94a
ayurn <ninaweayur@gmail.com>
2021-07-04 20:45:21 +0530

    [Ayur] Add : Python script to concatenate following dictionaries to create a new one.

---
+++ DataStructures/Dictionary/Concatinate.py
@@ -0,0 +1,25 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-03 18:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-03 18:15:30
+@Title : Write a Python script to concatenate following dictionaries to create a new one.
+        Sample Dictionary :
+        dic1={1:10, 2:20}
+        dic2={3:30, 4:40}
+        dic3={5:50,6:60}
+        Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+"""
+
+
+def concat():
+    dic1 = {1:10, 2:20}
+    dic2 = {3:30, 4:40}
+    dic3 = {5:50,6:60}
+    dct = dict()
+    for d in (dic1, dic2, dic3):
+        dct.update(d)
+    print("New Dictionary: ", dct)
+    
+if __name__ == '__main__':
+    concat()
