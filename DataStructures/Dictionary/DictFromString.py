ec734c6ac3a51a076213980710112918fcec384f
ayurn <ninaweayur@gmail.com>
2021-07-04 21:09:58 +0530

    [Ayur] Add : Python program to create a dictionary from a string.

---
+++ DataStructures/Dictionary/DictFromString.py
@@ -0,0 +1,20 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-03 18:30:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-03 18:30:30
+@Title : Write a Python program to create a dictionary from a string.
+        Note: Track the count of the letters from the string.
+        Sample string : 'w3resource'
+"""
+from collections import defaultdict, Counter
+
+def from_string():
+    str1 = 'w3resource' 
+    my_dict = {}
+    for letter in str1:
+        my_dict[letter] = my_dict.get(letter, 0) + 1
+    print(my_dict)
+
+if __name__ == '__main__':
+    from_string()
