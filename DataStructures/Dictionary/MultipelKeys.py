8f1845ed688e5a600a6162179f43ef33b3ee6363
ayurn <ninaweayur@gmail.com>
2021-07-05 10:36:36 +0530

    [Ayur] Add : Python program to check multiple keys exists in a dictionary.

---
+++ DataStructures/Dictionary/MultipelKeys.py
@@ -0,0 +1,25 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-04 20:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-04 20:15:30
+@Title :Write a Python program to check multiple keys exists in a dictionary.
+"""
+
+import loggerfile
+
+def keys():
+    try:
+        student = {
+        'name': 'Alex',
+        'class': 'V',
+        'roll_id': '2'
+        }
+        print(student.keys() >= {'class', 'name'})
+        print(student.keys() >= {'name', 'Alex'})
+        print(student.keys() >= {'roll_id', 'name'})
+    except Exception as e:
+        loggerfile.Logger("debug", print(e))
+        
+if __name__ == '__main__':
+    keys()
