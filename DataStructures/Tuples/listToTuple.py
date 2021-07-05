eff584725d568a70026ce22cf04ed5cb7824b1a7
ayurn <ninaweayur@gmail.com>
2021-07-06 00:06:10 +0530

    [Ayur] Add : Python program to convert list to tuple.

---
+++ DataStructures/Tuples/listToTuple.py
@@ -0,0 +1,32 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-05 21:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-05 21:15:30
+@Title : Write a Python program to convert a list to a tuple.
+"""
+
+import loggerfile
+
+def listToTuple():
+    
+    """
+    Description:
+        Function to convert a list to a tuple.
+    Parameter:
+        
+    Return:
+        
+    """
+    try:
+        #Convert list to tuple
+        listx = [5, 10, 7, 4, 15, 3]
+        print(listx)
+        #use the tuple() function built-in Python, passing as parameter the list
+        tuplex = tuple(listx)
+        print(tuplex)
+    except Exception :
+        loggerfile.Logger("debug", "Invalid program")
+        
+if __name__ == '__main__':
+    listToTuple() 
