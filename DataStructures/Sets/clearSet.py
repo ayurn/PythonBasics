bcd17381b12b783ab2c1056bfc9f46e0b73fd549
ayurn <ninaweayur@gmail.com>
2021-07-05 22:38:00 +0530

    [Ayur] Add : program  clear a set.

---
+++ DataStructures/Sets/clearSet.py
@@ -0,0 +1,32 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-05 20:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-05 20:15:30
+@Title : Write a Python program to clear a set.
+"""
+
+import loggerfile
+
+def clear():
+    """
+    Description:
+        Function to clear a set.
+    Parameter:
+        
+    Return:
+        
+    """
+    try:
+        # set of letters
+        charset = {'a', 'y', 'u', 'r', 'n'}
+        print('set before clear:', charset)
+        
+        # clearing vowels
+        charset.clear()
+        print('set after clear:', charset)
+    except Exception :
+       loggerfile.Logger("debug", "Invalid program")
+       
+if __name__ == '__main__':
+    clear() 
