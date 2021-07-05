2a4bb2e8306503df5c05665ffcd1bf4130665de9
ayurn <ninaweayur@gmail.com>
2021-07-05 11:06:56 +0530

    [Ayur] Add : Python program to print a dictionary in table format.

---
+++ DataStructures/Dictionary/TableDict.py
@@ -0,0 +1,20 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-04 18:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-04 18:15:30
+@Title : Write a Python program to print a dictionary in table format.
+"""
+
+import loggerfile
+
+def table():
+    try:
+        my_dict = {'C1':[1,2,3],'C2':[5,6,7],'C3':[9,10,11]}
+        for row in zip(*([key] + (value) for key, value in sorted(my_dict.items()))):
+            print(*row)
+    except Exception as e:
+        loggerfile.Logger("debug", print(e))
+        
+if __name__ == '__main__':
+    table()                    
