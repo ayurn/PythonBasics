aaf2fad02b5cb695207ca752ca1b31ff3f08cc4a
ayurn <ninaweayur@gmail.com>
2021-07-05 10:44:05 +0530

    [Ayur] Add : Python program to count number of items in a dictionary value that is a list.

---
+++ DataStructures/Dictionary/CountList.py
@@ -0,0 +1,31 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-04 20:30:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-04 20:30:30
+@Title : Write a Python program to count number of items in a dictionary value that is a list.
+
+"""
+
+import loggerfile
+
+def main():
+    try:
+        # defining the dictionary
+        d = {'A' : [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            'B' : 34,
+            'C' : 12,
+            'D' : [7, 8, 9, 6, 4] }
+    
+        # using the in operator
+        count = 0
+        for x in d:
+            if isinstance(d[x], list):
+                count += len(d[x])
+        print(count)
+    except Exception as e:
+        loggerfile.Logger("debug", print(e))
+  
+# Calling Main    
+if __name__ == '__main__':
+    main()
