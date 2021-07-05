73edbf6d308f26386018d5dd3b713c067bd1d3ac
ayurn <ninaweayur@gmail.com>
2021-07-05 11:12:10 +0530

    [Ayur] Add : Python program to count the values associated with key in a dictionary.

---
+++ DataStructures/Dictionary/CountValues.py
@@ -0,0 +1,23 @@
+ 
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-04 20:30:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-04 20:30:30
+@Title : Write a Python program to count the values associated with key in a dictionary.
+
+"""        
+import loggerfile
+        
+def countValue():
+    try:        
+        student = [{'id': 1, 'success': True, 'name': 'Lary'},
+        {'id': 2, 'success': False, 'name': 'Rabi'},
+        {'id': 3, 'success': True, 'name': 'Alex'}]
+        print(sum(d['id'] for d in student))
+        print(sum(d['success'] for d in student))
+    except Exception as e:
+        loggerfile.Logger("debug", print(e))
+        
+if __name__ == '__main__':
+    countValue()
