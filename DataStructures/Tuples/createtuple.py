739feaecb3b2f3e4dfc4b1ff2f98847d20f362a0
ayurn <ninaweayur@gmail.com>
2021-07-05 23:34:32 +0530

    [Ayur] Add : Python program to create tuple

---
+++ DataStructures/Tuples/createtuple.py
@@ -0,0 +1,29 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-05 21:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-05 21:15:30
+@Title : Write a Python program to create a tuple.
+"""
+
+import loggerfile
+
+def createTuple():
+    
+    """
+    Description:
+        Function to create create a tuple.
+    Parameter:
+        
+    Return:
+        
+    """
+    try:
+        #Create a tuple with different data types
+        tuplex = ("tuple", False, 3.2, 1)
+        print(tuplex)
+    except Exception :
+       loggerfile.Logger("debug", "Invalid program")
+       
+if __name__ == '__main__':
+    createTuple() 
