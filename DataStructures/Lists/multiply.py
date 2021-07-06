fcff448864c870b92fb0a90bd923c7384de97bbf
ayurn <ninaweayur@gmail.com>
2021-07-06 23:26:39 +0530

    [Ayur] Add : Python program to multiply all the items in list.

---
+++ DataStructures/Lists/multiply.py
@@ -0,0 +1,30 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-06 20:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-06 20:15:30
+@Title : Write a Python program to multipy all the items in a list.
+"""
+
+import loggerfile
+
+def multiply_list(items):
+    
+    """
+    Description:
+        Function to multipy all the items in a list.
+    Parameter:
+        
+    Return:
+        
+    """
+    try:
+        tot = 1
+        for x in items:
+            tot *= x
+        return tot
+    except Exception :
+        loggerfile.Logger("debug", "Invalid program")
+    
+if __name__ == '__main__':    
+    print(multiply_list([1,2,-8]))
