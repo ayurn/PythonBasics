8ec2b3239314a675011cfb117afe8a44dd5d3c30
ayurn <ninaweayur@gmail.com>
2021-07-06 18:13:42 +0530

    [Ayur] Add : changing last three characters of string

---
+++ DataStructures/Strings/addString.py
@@ -0,0 +1,30 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-06 14:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-06 14:15:30
+@Title : Write a Python program to add 'ing' at the end of a given string (length should be at
+least 3). If the given string already ends with 'ing' then add 'ly' instead. 
+If the string length of thegiven string is less than 3, leave it unchanged.
+"""
+
+import loggerfile
+
+def add_string(str1):
+    try:
+        length = len(str1)
+
+        if length > 2:
+            if str1[-3:] == 'ing':
+                str1 += 'ly'
+            else:
+                str1 += 'ing'
+
+        return str1
+    except Exception :
+        loggerfile.Logger("debug", "Invalid program")
+
+if __name__ == '__main__':
+    print(add_string('ab'))
+    print(add_string('abc'))
+    print(add_string('string'))
