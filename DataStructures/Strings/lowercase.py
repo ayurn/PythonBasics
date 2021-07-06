a9615f104add2aba2186bff1111c59a6c37415bf
ayurn <ninaweayur@gmail.com>
2021-07-06 23:14:49 +0530

    [Ayur] Add : Python program to lowercase first n characters in a string.

---
+++ DataStructures/Strings/lowercase.py
@@ -0,0 +1,28 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-06 14:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-06 14:15:30
+@Title : Write a Python program to lowercase first n characters in a string.
+"""
+
+import loggerfile
+
+def lowercase():
+    
+    """
+    Description:
+        Function to lowercase first n characters in a string.
+    Parameter:
+        
+    Return:
+        
+    """
+    try:
+
+        str1 = 'HAPPIESTMINDS.COM'
+        print(str1[:4].lower() + str1[4:])
+    except Exception :
+        loggerfile.Logger("debug", "Invalid program")
+
+lowercase()
