94f21a5a88c0c1d4505d530a9543b349e4039c11
ayurn <ninaweayur@gmail.com>
2021-07-06 23:20:20 +0530

    [Ayur] Add : Python program to do sum of all the items in a list.

---
+++ DataStructures/Lists/sum.py
@@ -0,0 +1,30 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-06 20:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-06 20:15:30
+@Title : Write a Python program to sum all the items in a list.
+"""
+
+import loggerfile
+
+def sum_list(items):
+    
+    """
+    Description:
+        Function to sum all the items in a list.
+    Parameter:
+        
+    Return:
+        
+    """
+    try:
+        sum_numbers = 0
+        for x in items:
+            sum_numbers += x
+        return sum_numbers
+    except Exception :
+        loggerfile.Logger("debug", "Invalid program")
+    
+if __name__ == '__main__':    
+    print(sum_list([1,2,-8]))
