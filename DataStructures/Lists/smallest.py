25f74a8ee9206c4b9ba0ff83583998260efce65f
ayurn <ninaweayur@gmail.com>
2021-07-06 23:32:18 +0530

    [Ayur] Add : Python program to find smallest element in list.

---
+++ DataStructures/Lists/smallest.py
@@ -0,0 +1,31 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-06 20:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-06 20:15:30
+@Title : Write a Python program to get the smallest number from a list.
+"""
+
+import loggerfile
+
+def smallest_num_in_list( list ):
+    
+    """
+    Description:
+        Function to get the smallest number from a list.
+    Parameter:
+        
+    Return:
+        
+    """
+    try:
+        min = list[ 0 ]
+        for a in list:
+            if a < min:
+                min = a
+        return min
+    except Exception :
+        loggerfile.Logger("debug", "Invalid program")
+    
+if __name__ == '__main__':    
+    print(smallest_num_in_list([1, 2, -8, 0]))
