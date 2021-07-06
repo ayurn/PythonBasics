ad9962929dea4f8acfb4fb0d73cc37066be209c2
ayurn <ninaweayur@gmail.com>
2021-07-07 00:13:18 +0530

    [Ayur] Add : Python program to remove duplicate items from list.

---
+++ DataStructures/Lists/duplicate.py
@@ -0,0 +1,33 @@
+"""
+@Author: Ayur Ninawe
+@Date: 2021-07-06 20:15:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-06 20:15:30
+@Title : Write a Python program to remove duplicates from a list.
+"""
+
+import loggerfile
+
+def Duplicate():
+    
+    """
+    Description:
+        Function to remove duplicates from a list.
+    Parameter:
+    Return:
+    """
+    try:
+        a = [10,20,30,20,10,50,60,40,80,50,40]
+
+        dup_items = set()
+        uniq_items = []
+        for x in a:
+            if x not in dup_items:
+                uniq_items.append(x)
+                dup_items.add(x)
+
+        print(dup_items)
+    except Exception :
+        loggerfile.Logger("debug", "Invalid program")
+ 
+Duplicate()
